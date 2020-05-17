9b04b1530e3214a62f793f3a3e8f6ab4281ea628
zyjcyg <zyjcyg@163.com>
2020-05-17 21:58:06 +0800

    test

--- zjy-code-00-test/src/main/java/Main.java
+++ zjy-code-00-test/src/main/java/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+		System.out.println("hello");
         while(scanner.hasNextInt())
         {
             int n = scanner.nextInt();
